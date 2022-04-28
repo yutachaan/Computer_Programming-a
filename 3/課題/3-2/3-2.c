e3345dcd20faf9acebef2e48943d251ffc54d4de
yutachaan <yutachan010907@gmail.com>
2022-04-28 17:18:25 +0900

    :sparkles: 関数ポインタまわり設定完了

--- 3/課題/3-2/3-2.c
+++ 3/課題/3-2/3-2.c
@@ -3,6 +3,9 @@
 #include <math.h>
 
 
+typedef double (*P_FUNC)(double);
+
+
 // target function 1
 double f1(double x) {
   return x * x;
@@ -24,7 +27,7 @@ double trapezoid(double w1, double w2, double h) {
 }
 
 // 台形法により一変数関数の積分値を計算
-double integral(double x1, double x2, int n) {
+double integral(double x1, double x2, int n, P_FUNC funcptr) {
   double ret;
 
   return ret;
@@ -32,10 +35,10 @@ double integral(double x1, double x2, int n) {
 
 
 int main() {
-  // double ?????? = { f1, f2, f3 };
+  P_FUNC funcptr[3] = {f1, f2, f3};                                  // 積分値を求める関数の関数ポインタの配列
   double val[3][2] = {{1.0, 10.0}, {1.0, 10.0}, {M_PI / 3.0, M_PI}}; // 積分範囲
 
   for (int i = 0; i < 3; i++) {
-    printf("f%d: %10f\n", i, integral(val[i][0], val[i][1], 10000));
+    printf("f%d: %10f\n", i, integral(val[i][0], val[i][1], 10000, funcptr[i]));
   }
 }
