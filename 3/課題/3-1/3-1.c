47da73b41f3e7924e0267e620418a90c2941f469
yutachaan <yutachan010907@gmail.com>
2022-05-10 18:50:58 +0900

    :recycle: エラー周りの処理

--- 3/課題/3-1/3-1.c
+++ 3/課題/3-1/3-1.c
@@ -5,28 +5,40 @@
 // データの特徴次元毎の平均からなる平均ベクトルを計算
 double *get_mean(int ndim, double **data, int ndata) {
   // 領域確保
-  double *ret;
-  if ((ret = (double*)malloc(ndim * sizeof(double))) == NULL) exit(1);
+  double *mean;
+  if ((mean = (double*)malloc(ndim * sizeof(double))) == NULL) {
+    printf("Memory Allocation Error\n");
+    exit(EXIT_FAILURE);
+  }
 
   // 特徴次元ごとに平均を求める
   for (int j = 0; j < ndim; j++) {
-    for (int i = 0; i < ndata; i++) ret[j] += data[i][j];
-    ret[j] /= ndata;
+    for (int i = 0; i < ndata; i++) mean[j] += data[i][j];
+    mean[j] /= ndata;
   }
 
-  return ret;
+  return mean;
 }
 
 // 特徴次元間の分散共分散行列を計算
 double **get_cova(int ndim, double **data, int ndata) {
   // 領域確保
   double *mean;
-  if ((mean = (double*)malloc(ndim * sizeof(double))) == NULL) exit(1);
+  if ((mean = (double*)malloc(ndim * sizeof(double))) == NULL) {
+    printf("Memory Allocation Error\n");
+    exit(EXIT_FAILURE);
+  }
 
-  double **ret;
-  if ((ret = (double**)malloc(ndim * sizeof(double*))) == NULL) exit(1);
+  double **cova;
+  if ((cova = (double**)malloc(ndim * sizeof(double*))) == NULL) {
+    printf("Memory Allocation Error\n");
+    exit(EXIT_FAILURE);
+  }
   for (int i = 0; i < ndim; i++) {
-    if ((ret[i] = (double*)malloc(ndim * sizeof(double))) == NULL) exit(1);
+    if ((cova[i] = (double*)malloc(ndim * sizeof(double))) == NULL) {
+      printf("Memory Allocation Error\n");
+      exit(EXIT_FAILURE);
+    }
   }
 
   // 平均を求める
@@ -36,19 +48,22 @@ double **get_cova(int ndim, double **data, int ndata) {
   for (int i = 0; i < ndim; i++) {
     for (int j = 0; j < ndim; j++) {
       for (int k = 0; k < ndata; k++) {
-        ret[i][j] += (data[k][i] - mean[i]) * (data[k][j] - mean[j]);
+        cova[i][j] += (data[k][i] - mean[i]) * (data[k][j] - mean[j]);
       }
-      ret[i][j] /= ndata;
+      cova[i][j] /= ndata;
     }
   }
 
-  return ret;
+  return cova;
 }
 
 
 int main(int argc, char *argv[]) {
   FILE *file;
-  if ((file = fopen(argv[1], "r")) == NULL) exit(1);
+  if ((file = fopen(argv[1], "r")) == NULL) {
+    printf("File Open Error: %s\n", file);
+    exit(EXIT_FAILURE);
+  }
 
   // フレーム数と特徴次元数を入力
   int ndata, ndim;
@@ -57,9 +72,15 @@ int main(int argc, char *argv[]) {
 
   // 領域確保
   double **data;
-  if ((data = (double**)malloc(ndata * sizeof(double*))) == NULL) exit(1);
+  if ((data = (double**)malloc(ndata * sizeof(double*))) == NULL) {
+    printf("Memory Allocation Error\n");
+    exit(EXIT_FAILURE);
+  }
   for (int i = 0; i < ndata; i++) {
-    if ((data[i] = (double*)malloc(ndim * sizeof(double))) == NULL) exit(1);
+    if ((data[i] = (double*)malloc(ndim * sizeof(double))) == NULL) {
+      printf("Memory Allocation Error\n");
+      exit(EXIT_FAILURE);
+    }
   }
 
 
@@ -73,11 +94,14 @@ int main(int argc, char *argv[]) {
 
   // 平均ベクトルを求める
   double *mean;
-  if ((mean = (double*)malloc(ndim * sizeof(double))) == NULL) exit(1);
+  if ((mean = (double*)malloc(ndim * sizeof(double))) == NULL) {
+    printf("Memory Allocation Error\n");
+    exit(EXIT_FAILURE);
+  }
 
   mean = get_mean(ndim, data, ndata);
 
-  printf("mean vector: \n");
+  printf("mean vector:\n");
   for (int j = 0; j < ndim; j++) printf("%10lf\n", mean[j]);
   printf("\n");
 
@@ -86,14 +110,20 @@ int main(int argc, char *argv[]) {
 
   // 分散共分散行列を求める
   double **cova;
-  if ((cova = (double**)malloc(ndim * sizeof(double*))) == NULL) exit(1);
+  if ((cova = (double**)malloc(ndim * sizeof(double*))) == NULL) {
+    printf("Memory Allocation Error\n");
+    exit(EXIT_FAILURE);
+  }
   for (int i = 0; i < ndim; i++) {
-    if ((cova[i] = (double*)malloc(ndim * sizeof(double))) == NULL) exit(1);
+    if ((cova[i] = (double*)malloc(ndim * sizeof(double))) == NULL) {
+      printf("Memory Allocation Error\n");
+      exit(EXIT_FAILURE);
+    }
   }
 
   cova = get_cova(ndim, data, ndata);
 
-  printf("covariance matrix: \n");
+  printf("covariance matrix:\n");
   for (int i = 0; i < ndim; i++) {
     for (int j = 0; j < ndim - 1; j++) printf("%10lf ", cova[i][j]);
     printf("%10lf\n", cova[i][ndim - 1]);
