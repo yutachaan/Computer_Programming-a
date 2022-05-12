ff963dfb65f8236c5b940432c55214be241ab63b
yutachaan <yutachan010907@gmail.com>
2022-05-12 22:17:07 +0900

    :sparkles: 4-2動いた

--- 4/課題/4-2/4-2.c
+++ 4/課題/4-2/4-2.c
@@ -13,58 +13,113 @@ typedef struct list {
   struct list *next;  // 次の要素へのポインタ
 } LIST, *LISTP;
 
-// ノードのメモリを確保し，値を代入
-LISTP mallocNode(char *word) {
-  LISTP p;
+// リストの末尾にノードを追加
+void appendList(LIST **head, char *word) {
+  LISTP new, p = *head, prev;
 
   // メモリ確保
-  if ((p = (LISTP)malloc(sizeof(LIST))) == NULL) {
+  if ((new = (LISTP)malloc(sizeof(LIST))) == NULL) {
     printf("Memory Allocation Error\n");
     exit(EXIT_FAILURE);
   }
 
   // 値の代入
-  strcpy(p->word, word);
-  p->count = 1;
-  p->next = NULL;
+  strcpy(new->word, word);
+  new->count = 1;
+  new->next = NULL;
 
-  return p;
-}
-
-// リストの末尾にノードを追加
-LISTP appendList(LISTP head, char *word) {
-  LISTP new, p = head;
-
+  // まだ要素が1つもないなら先頭に追加
   if (p == NULL) {
-    p = mallocNode(word);
-    return p;
+    *head = new;
+    return;
   }
 
-  if (strcmp(word, p->word) == 0) p->count++; // 等しい場合はカウントアップ
-  else p->next = appendList(p->next, word);   // それ以外の場合は次の要素へ
+  // 末尾までポインタを移動
+  while (p != NULL) {
+    // すでに登録されているならカウントアップ
+    if (strcmp(p->word, word) == 0) {
+      p->count++;
+      return;
+    }
 
-  return p;
+    prev = p;
+    p = p->next;
+  }
+
+  // 末尾の要素の次の位置に追加
+  prev->next = new;
+
+  return;
 }
 
 // リストの要素を順に表示
-void printList(LISTP p) {
-  int n = 1;
+void printList(LIST **head, int *n) {
+  LISTP p = *head;
+
   while (p != NULL) {
-    printf("%d: WORD: %s, COUNT: %d\n", n, p->word, p->count);
+    printf("%d: WORD: %s, COUNT: %d\n", *n, p->word, p->count);
 
     p = p->next;
-    n++;
+    (*n)++;
   }
 
   return;
 }
 
 // リストのメモリ解放
-void freeList(LISTP p) {
+void freeList(LIST **head) {
+  LISTP p = *head;
+
   if (p == NULL) return;
 
-  freeList(p->next);
+  freeList(&(p->next));
   free(p);
+
+  return;
+}
+
+
+// ハッシュ関数
+int hash(char *str) {
+  int h = 0;
+  for (int i = 0; str[i] != '\0'; i++) {
+    h = (h * 137 + str[i]) % HASHSIZE;
+  }
+
+  return h;
+}
+
+// ハッシュテーブルに値を追加
+void insertHash(LIST **hash_table, char *word) {
+  int h = hash(word);
+
+  if (h >= HASHSIZE) {
+    printf("Hash Value Error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // ハッシュ値の位置のリストに追加
+  appendList(&hash_table[h], word);
+
+  return;
+}
+
+// ハッシュ表の先頭から要素を表示
+void printHash(LIST **hash_table) {
+  int n = 1;
+
+  for (int i = 0; i < HASHSIZE; i++) {
+    printList(&hash_table[i], &n);
+  }
+
+  return;
+}
+
+// ハッシュ表のメモリ解放
+void freeHash(LIST **hash_table) {
+  for (int i = 0; i < HASHSIZE; i++) {
+    freeList(&hash_table[i]);
+  }
 
   return;
 }
@@ -96,22 +151,25 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
+  // ハッシュテーブルを初期化
+  LISTP hash_table[HASHSIZE];
+  for (int i = 0; i < HASHSIZE; i++) hash_table[i] = NULL;
+
   char str[STRLEN];
-  LISTP list = NULL;
   while(fgets(str,sizeof(str), file) != NULL) {
     // 文字列を分割
     char *result[STRLEN];
     int count = split(str, " \n", result); // 空白と改行コードでsplit
 
     // 線形リストに追加
-    for (int i = 0; i < count; i++) list = appendList(list, result[i]);
+    for (int i = 0; i < count; i++) insertHash(hash_table, result[i]);
   }
 
   // 線形リストを順に出力
-  printList(list);
+  printHash(hash_table);
 
   // メモリ解放
-  freeList(list);
+  freeHash(hash_table);
 
   fclose(file);
 }
