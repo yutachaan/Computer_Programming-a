b2efdca6a013e04e517f46a2eac2c7f2c48cd1f5
yutachaan <yutachan010907@gmail.com>
2022-05-30 22:48:03 +0900

    :sparkles: owari

--- 6/課題/6-1/6-1.c
+++ 6/課題/6-1/6-1.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <limits.h>
 
 // エラー処理を含めたmalloc()関数
 void *MyMalloc(size_t size) {
@@ -17,17 +18,49 @@ void *MyMalloc(size_t size) {
 
 // 力まかせ法
 void Naive(int filesize, char *text, char *pattern) {
-  int n = 0, len = strlen(pattern);
+  int count = 0, pattern_len = strlen(pattern);
 
   clock_t t1 = clock();
   for (int i = 0; i < filesize; i++) {
-    if (strncmp(text + i, pattern, len) == 0) n++;
+    if (strncmp(text + i, pattern, pattern_len) == 0) count++;
   }
   clock_t t2 = clock();
 
-  printf("\"%s\" found %d times!\n", pattern, n);
+  printf("\"%s\" found %d times!\n", pattern, count);
   printf("Used CPU time: %.10f\n", (double)(t2 - t1) / CLOCKS_PER_SEC);
 }
+
+// BM法
+void BM(int filesize, char *text, char *pattern) {
+  int count = 0, pattern_len = strlen(pattern);
+  int skip[UCHAR_MAX]; // スキップテーブル
+
+  clock_t t1 = clock();
+
+  // スキップテーブルの作成
+  for (int c = 0; c < UCHAR_MAX; c++) skip[c] = pattern_len;
+  for (int i = 0; i < pattern_len; i++) skip[pattern[i]] = pattern_len - i - 1;
+
+  int text_pos = pattern_len - 1, pattern_pos = pattern_len - 1;
+  while (text_pos < filesize) {
+    while (text[text_pos] == pattern[pattern_pos]) {
+      if (pattern_pos == 0) count++;
+      text_pos--;
+      pattern_pos--;
+    }
+
+    if (skip[text[text_pos]] > pattern_len - 1 - pattern_pos) text_pos += skip[text[text_pos]];
+    else text_pos += pattern_len - pattern_pos;
+
+    pattern_pos = pattern_len - 1;
+  }
+
+  clock_t t2 = clock();
+
+  printf("\"%s\" found %d times!\n", pattern, count);
+  printf("Used CPU time: %.10f\n", (double)(t2 - t1) / CLOCKS_PER_SEC);
+}
+
 
 int main(int argc, char *argv[]) {
   FILE *fp;
@@ -50,7 +83,12 @@ int main(int argc, char *argv[]) {
   char *pattern = argv[2];
 
   // 力まかせ法
+  printf("Naive:\n");
   Naive(fsize, text, pattern);
+
+  // BM法
+  printf("BM:\n");
+  BM(fsize, text, pattern);
 
   free(text);
 
